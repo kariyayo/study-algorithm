1fa2c525143abf2812b771f7017253673222ceff
bati11 <mail.bati11@gmail.com>
2015-02-03 23:15:20 +0900

    Add remove method in binary search tree

--- catalog/binary_search_tree/binary_tree.rb
+++ catalog/binary_search_tree/binary_tree.rb
@@ -7,16 +7,45 @@ class Node
 
   def insert(v)
     if v < @v
-      if @left.instance_of? Empty
+      if @left.empty?
         @left = Node.new(v, Empty.new, Empty.new)
       else
         @left.insert(v)
       end
     elsif v > @v
-      if @right.instance_of? Empty
+      if @right.empty?
         @right = Node.new(v, Empty.new, Empty.new)
       else
         @right.insert(v)
+      end
+    end
+    self
+  end
+
+  def remove(v)
+    if v < @v
+      if v == @left.v
+        if @left.has_both?
+          max = find_max_value(@left.right)
+          @left.remove(max)
+          @left.v = max
+        else
+          @left = child_or_empty(@left)
+        end
+      else
+        @left.remove(v)
+      end
+    elsif v > @v
+      if v == @right.v
+        if @right.has_both?
+          max = find_max_value(@right.right)
+          @right.remove(max)
+          @right.v = max
+        else
+          @right = child_or_empty(@right)
+        end
+      else
+        @right.remove(v)
       end
     end
     self
@@ -32,8 +61,53 @@ class Node
     end
   end
 
-  def to_s
-    "Node(#{@v}, #{@left.to_s}, #{@right.to_s})"
+  def has_both?
+    !@left.empty? && !@right.empty?
+  end
+
+  def no_child?
+    @left.empty? && @right.empty?
+  end
+
+  def left_only?
+    !@left.empty? && @right.empty?
+  end
+
+  def right_only?
+    @left.empty? && !@right.empty?
+  end
+
+  def empty?
+    self.instance_of? Empty
+  end
+
+  def inspect
+    "Node(#{@v}, #{@left.inspect}, #{@right.inspect})"
+  end
+
+private
+
+  def child_or_empty(node)
+    if node.no_child?
+      Empty.new
+    elsif node.right_only?
+      node.right
+    elsif node.left_only?
+      node.left
+    end
+  end
+
+
+  def find_max_value(node)
+    if node.left.empty? && node.right.empty?
+      node.v
+    elsif node.left.empty?
+      [node.v, find_max_value(node.right)].max
+    elsif node.right.empty?
+      [node.v, find_max_value(node.left)].max
+    else
+      [node.v, find_max_value(node.left), find_max_value(node.right)].max
+    end
   end
 end
 
@@ -42,7 +116,11 @@ class Empty
     false
   end
 
-  def to_s
+  def empty?
+    self.instance_of? Empty
+  end
+
+  def inspect
     "Empty"
   end
 end
