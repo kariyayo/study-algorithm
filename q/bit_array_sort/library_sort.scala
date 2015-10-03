d08f4fb1ca0c1feda11252fe1f39d389066a2d20
bati11 <mail.bati11@gmail.com>
2015-10-03 17:35:12 +0900

    Move into main method

--- q/bit_array_sort/library_sort.scala
+++ q/bit_array_sort/library_sort.scala
@@ -1,5 +1,9 @@
-val file = new java.io.PrintWriter("out/output_library_sort")
-var source = scala.io.Source.fromFile("out/testdata")
-source.getLines.toSeq.sorted.foreach(file.println)
-source.close()
-file.close()
+object Main {
+  def main(args: Array[String]): Unit = {
+    val file = new java.io.PrintWriter("out/output_library_sort")
+    var source = scala.io.Source.fromFile("out/testdata")
+    source.getLines.toSeq.sorted.foreach(file.println)
+    source.close()
+    file.close()
+  }
+}
