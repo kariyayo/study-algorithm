9241c5bbd7e108b8d15007c115fda6299d8d0a47
bati11 <mail.bati11@gmail.com>
2015-10-03 17:50:30 +0900

    Convert from String to Int before sorting

--- q/bit_array_sort/library_sort.scala
+++ q/bit_array_sort/library_sort.scala
@@ -2,7 +2,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     val file = new java.io.PrintWriter("out/output_library_sort")
     var source = scala.io.Source.fromFile("out/testdata")
-    source.getLines.toSeq.sorted.foreach(file.println)
+    source.getLines.map(_.toInt).toSeq.sorted.foreach(x => file.println(f"${x}%07d"))
     source.close()
     file.close()
   }
