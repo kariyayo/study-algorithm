b2dd5bc690a44e02271c553f47485f4f150d35e2
bati11 <mail.bati11@gmail.com>
2021-05-30 23:57:08 +0900

    add ABC203

---
+++ at_coder/abc/abc203/B/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, 0)
+	sc.Buffer(buf, 1010000)
+	sc.Split(bufio.ScanWords)
+
+	N := nextInt(sc)
+	K := nextInt(sc)
+
+	ans := 0
+	for i := 1; i <= N; i++ {
+		for j := 1; j <= K; j++ {
+			x, err := strconv.Atoi(fmt.Sprintf("%d0%d", i, j))
+			if err != nil {
+				panic(err)
+			}
+			ans = ans + x
+		}
+	}
+	fmt.Println(ans)
+}
+
+// ----------
+type Pair struct {
+	First  string
+	Second int
+}
+
+type IntSet map[int]struct{}
+
+func NewIntSet() IntSet {
+	return map[int]struct{}{}
+}
+func (set IntSet) append(x int) {
+	set[x] = struct{}{}
+}
+
+type StringSet map[string]struct{}
+
+func NewStringSet() StringSet {
+	return map[string]struct{}{}
+}
+func (set StringSet) append(s string) {
+	set[s] = struct{}{}
+}
+
+func toInt(s string) int {
+	x, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func toInt64(s string) uint64 {
+	x, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return x
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minUint64(a, b uint64) uint64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func abs(a int) int {
+	return int(math.Abs(float64(a)))
+}
+
+func pascalTriangle(n int) [][]int {
+	pt := make([][]int, n+1)
+	for i := 0; i < n+1; i++ {
+		pt[i] = make([]int, i+1)
+		pt[i][0] = 1
+		pt[i][i] = 1
+		for j := 1; j < i; j++ {
+			pt[i][j] = pt[i-1][j-1] + pt[i-1][j]
+		}
+	}
+	return pt
+}
+
+func nextString(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextNumber(sc *bufio.Scanner) float64 {
+	sc.Scan()
+	f, err := strconv.ParseFloat(sc.Text(), 32)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func nextInt(sc *bufio.Scanner) int {
+	sc.Scan()
+	n, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func nextUint64(sc *bufio.Scanner) uint64 {
+	sc.Scan()
+	n, err := strconv.ParseUint(sc.Text(), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func printArray(xs []int) {
+	fmt.Println(strings.Trim(fmt.Sprint(xs), "[]"))
+}
+
+func debugPrintArray(xs []int) {
+	fmt.Fprintln(os.Stderr, strings.Trim(fmt.Sprint(xs), "[]"))
+}
+
+func debugPrintTable(table [][]int) {
+	for i := 0; i < len(table); i++ {
+		debugPrintArray(table[i])
+	}
+}
+
+func debugPrintf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+}
