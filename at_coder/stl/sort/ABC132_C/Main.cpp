beb6e0372d5c1556422f04b1804550697c543c0f
bati11 <mail.bati11@gmail.com>
2022-08-04 21:43:50 +0900

    add stl/sort

---
+++ at_coder/stl/sort/ABC132_C/Main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+template <typename T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template <typename T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> void printVector(vector<T> xs, string prefix = "[ ", string suffix = " ]");
+
+const long long INF_LL = 1LL<<60;
+const int INF = 1000010000;
+
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> ds(N);
+  for (int i = 0; i < N; i++) cin >> ds[i];
+  sort(ds.begin(), ds.end());
+  int a = ds[N/2 - 1];
+  int b = ds[N/2];
+  cout << b - a << endl;
+}
+
+template <typename T>
+void printVector(vector<T> xs, string prefix, string suffix) {
+  cout << prefix;
+  for (int i = 0; i < (ll)xs.size(); i++) {
+    cout << xs[i];
+    if (i+1 != (ll)xs.size()) {
+      cout << " ";
+    }
+  }
+  cout << suffix << endl;
+}
---
+++ at_coder/stl/sort/AGC012_A/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+template <typename T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template <typename T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> void printVector(vector<T> xs, string prefix = "[ ", string suffix = " ]");
+
+const long long INF_LL = 1LL<<60;
+const int INF = 1000010000;
+
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> as(3*N);
+  for (int i = 0; i < 3*N; i++) cin >> as[i];
+  sort(as.begin(), as.end(), greater<int>());
+  ll ans = 0;
+  for (int i = 0; i < 2*N; i++) {
+    if (i % 2 == 1) {
+      ans += as[i];
+    }
+  }
+  cout << ans << endl;
+}
+
+template <typename T>
+void printVector(vector<T> xs, string prefix, string suffix) {
+  cout << prefix;
+  for (int i = 0; i < (ll)xs.size(); i++) {
+    cout << xs[i];
+    if (i+1 != (ll)xs.size()) {
+      cout << " ";
+    }
+  }
+  cout << suffix << endl;
+}
