93d52c0f340ea44e3f3846894e62ccb7f4cf56ac
bati11 <mail.bati11@gmail.com>
2022-08-19 23:00:51 +0900

    update ABC077_C

--- at_coder/Binary_Search/ABC077_C_SnukeFestival/main.cpp
+++ at_coder/Binary_Search/ABC077_C_SnukeFestival/main.cpp
@@ -1,51 +1,47 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
-
 using namespace std;
-
 using uint = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
-
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define rep2(i, start, n) for (int i = (start); i < (int)(n); ++i)
-
 template <typename T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 template <typename T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
-template <typename T> void printVector(vector<T> xs);
+template <typename T> void printVector(vector<T> xs, string prefix = "[ ", string suffix = " ]");
 
-// const long long INF = 1LL<<60;
+const long long INF_LL = 1LL<<60;
 const int INF = 1000010000;
+
 
 int main() {
   int N;
   cin >> N;
-  vector<int> as(N), bs(N), cs(N);
-  rep(i, N) cin >> as[i];
-  rep(i, N) cin >> bs[i];
-  rep(i, N) cin >> cs[i];
-
+  vector<int> as(N);
+  vector<int> bs(N);
+  vector<int> cs(N);
+  for (int i = 0; i < N; i++) cin >> as[i];
+  for (int i = 0; i < N; i++) cin >> bs[i];
+  for (int i = 0; i < N; i++) cin >> cs[i];
   sort(as.begin(), as.end());
   sort(cs.begin(), cs.end());
+
   ll ans = 0;
-  rep(i, N) {
+  for (int i = 0; i < N; i++) {
     int b = bs[i];
-    auto ai_iter = lower_bound(as.begin(), as.end(), b);
-    size_t ai = distance(as.begin(), ai_iter);
-    auto ci_iter = upper_bound(cs.begin(), cs.end(), b);
-    size_t ci = distance(cs.begin(), ci_iter);
-    ll cnt = ai * (N-ci);
-    ans += cnt;
+    ll aCnt = lower_bound(as.begin(), as.end(), b) - as.begin();
+    ll cCnt = cs.end() - upper_bound(cs.begin(), cs.end(), b);
+    ans += aCnt * cCnt;
   }
   cout << ans << endl;
 }
 
-
 template <typename T>
-void printVector(vector<T> xs) {
-  cout << '[';
-  for (auto& x: xs) {
-    cout << x << ' ';
+void printVector(vector<T> xs, string prefix, string suffix) {
+  cout << prefix;
+  for (int i = 0; i < (ll)xs.size(); i++) {
+    cout << xs[i];
+    if (i+1 != (ll)xs.size()) {
+      cout << " ";
+    }
   }
-  cout << ']' << endl;
+  cout << suffix << endl;
 }
